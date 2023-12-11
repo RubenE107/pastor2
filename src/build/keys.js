5a0e1a13be0532f90c053857fa95659392e2b5d8
Ruben <irtu098@gmail.com>
2023-12-11 11:20:29 -0600

    first commit

---
+++ src/build/keys.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    database: {
+        host: process.env.DB_HOST || 'localhost',
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASSWORD || '',
+        database: process.env.DB_DATABASE || 'Blink',
+    }
+};
